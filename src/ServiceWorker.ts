700a155a71c7dbea035ed302b5de3332fadac522
Victor Ivanov <vi.ivanov@vk.team>
2023-10-05 12:52:25 +0300

    afdsasdfsfsaff

--- src/ServiceWorker.ts
+++ src/ServiceWorker.ts
@@ -105,7 +105,7 @@ function checkValidServiceWorker(swUrl: string, config?: Config) {
 
 export function register(config?: Config) {
   window.addEventListener('load', () => {
-    const swUrl = '/service-worker.js';
+    const swUrl = './service-worker.js';
 
     if (isLocalhost) {
       // This is running on localhost. Let's check if a service worker still exists or not.
