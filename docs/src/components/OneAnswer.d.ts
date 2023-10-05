c545a8f61b4db2d5ba6f25589181a52b457ea6b8
Victor Ivanov <vi.ivanov@vk.team>
2023-10-05 17:30:25 +0300

    dffd

---
+++ docs/src/components/OneAnswer.d.ts
@@ -0,0 +1,7 @@
+/// <reference types="react" />
+declare const OneAnswer: (props: {
+    count: number;
+    text: string;
+    theme: 'blue' | 'yellow';
+}) => JSX.Element;
+export default OneAnswer;
---
+++ src/components/OneAnswer.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+
+const OneAnswer = (props: {count: number, text: string, theme: 'blue' | 'yellow'}) => {
+  const {
+    text,
+    count,
+    theme,
+  } = props;
+
+  return (
+    <div>
+      {text}
+      <div className={`oneAnswer oneAnswer__${theme}`}>
+        <div>
+          {count}
+          <br />
+          <span>дней</span>
+        </div>
+        <img src={`./${theme}Cat.svg`} alt="cat" />
+      </div>
+    </div>
+  );
+};
+
+export default OneAnswer;
